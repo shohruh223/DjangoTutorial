e65bac49c7b479be3eff0859c9af9a303f71db46
shohruh223 <shohruh.abd2223@gmail.com>
2023-06-04 11:51:18 +0500

    add send_sms

--- app/views.py
+++ app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from twilio.rest import Client
 
-# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        text = request.POST['text']
+        if text:
+            account_sid = "your account_sid"
+            auth_token = "your auth_token"
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(
+                to="+998334530919",
+                from_="+13613093414",
+                body=f"Sizga {name} SMS jo'natdi: {text}")
+            return redirect('/')
+        else:
+            return redirect('/')
+    return render(request, 'app/index.html')
+
+
