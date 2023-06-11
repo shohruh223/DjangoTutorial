1ce6c73a7bf210ac11371ee163e1137f1471b092
shohruh223 <shohruh.abd2223@gmail.com>
2023-06-11 15:31:56 +0500

    django adminn

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.title
